8f43a0f7a7b2bae66617202efa6be7338bf10a22
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-08-19 18:44:34 -0300

    Change size in whatsapp button and banner

--- components/banner/banner_container/index.tsx
+++ components/banner/banner_container/index.tsx
@@ -2,7 +2,7 @@ import {ReactNode} from "react";
 
 export function BannerContainer ({ children } : { children : ReactNode}) {
     return (
-        <div className="w-screen min-h-[550px] max-h-[700px] relative">
+        <div className="w-screen min-h-[600px] max-h-[700px] relative">
             {children}
         </div>
     )
--- components/whatsapp_float_button/index.tsx
+++ components/whatsapp_float_button/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function WhatsAppFloatButton(){
     return (
-        <div className="h-20 w-20 rounded-full right-3 bottom-2 cursor-pointer overflow-hidden fixed">
+        <div className="h-16 w-16 rounded-full right-3 bottom-2 cursor-pointer overflow-hidden fixed">
             <Image alt={"Icone do whatsapp"} src={"whatsapp_logo.svg"} fill className="object-cover"/>
         </div>
     )
