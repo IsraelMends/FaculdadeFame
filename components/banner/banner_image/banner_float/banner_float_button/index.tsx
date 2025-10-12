4f3a48678cde88cade5f678a0b48afdf7389ab6e
Israel Mends <israel.mendes@a.unileste.edu.br>
2025-10-12 13:08:21 -0300

    feat: Change the button to a link that send to the contact component

--- components/banner/banner_image/banner_float/banner_float_button/index.tsx
+++ components/banner/banner_image/banner_float/banner_float_button/index.tsx
@@ -1,10 +1,12 @@
-
+import Link from "next/link"
 export function BannerFloatButton() {
     return (
-        <button className="mt-12 bg-[var(--black-green)] p-4 w-72 h-16 text-2xl font-sans font-medium rounded-xl shadow-md shadow-[#2fddc0] text-white cursor-pointer
+        <Link
+            href="#contato"
+            className="mt-12 bg-[var(--black-green)] p-4 w-72 h-16 text-2xl font-sans font-medium rounded-xl shadow-md shadow-[#2fddc0] text-white cursor-pointer
                 hover:scale-105
                 ">
             Matricule-se
-        </button>
+        </Link>
     )
 }
