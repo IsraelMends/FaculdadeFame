fe611c56438cbd9fe3e2529965b0a2fba2f17f66
Israel Mends <israel.mendes@a.unileste.edu.br>
2025-10-12 13:08:48 -0300

    style: Adjust formatting of imports and components on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-import {Header} from "@/components/header";
-import {Banner} from "@/components/banner";
-import {Carousel} from "@/components/carousel";
-import {WhatsAppFloatButton} from "@/components/whatsapp_float_button";
-import {About} from "@/components/about";
-import {Footer} from "@/components/footer";
-import {ContactForm} from "@/components/form";
+import { Header } from "@/components/header";
+import { Banner } from "@/components/banner";
+import { Carousel } from "@/components/carousel";
+import { WhatsAppFloatButton } from "@/components/whatsapp_float_button";
+import { About } from "@/components/about";
+import { Footer } from "@/components/footer";
+import { ContactForm } from "@/components/form";
 
 export default function Home() {
     return (
         <div className="w-full flex flex-col min-h-screen h-auto">
-            <Header/>
-            <Banner/>
-            <About/>
-            <Carousel/>
+            <Header />
+            <Banner />
+            <About />
+            {/* <Carousel/> */}
             <ContactForm />
             <Footer />
-            <WhatsAppFloatButton/>
+            <WhatsAppFloatButton />
         </div>
     );
 }
