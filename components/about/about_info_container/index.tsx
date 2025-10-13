dbe2be85cd489326585ab02e7c0347b0329cfd7d
IsraelMends <iomendes505@gmail.com>
2025-10-13 08:48:24 -0300

    style: Update layout and spacing in AboutContainer, AboutDescription, and AboutInfoContainer components

--- components/about/about_info_container/index.tsx
+++ components/about/about_info_container/index.tsx
@@ -2,7 +2,7 @@ import {ReactNode} from "react";
 
 export function AboutInfoContainer({children}: {children: ReactNode}) {
     return (
-        <div className="w-8/12 h-[600px] flex rounded-2xl">
+        <div className="w-full max-w-6xl h-auto md:h-[600px] flex rounded-2xl px-2 md:px-0">
             {children}
         </div>
     )
