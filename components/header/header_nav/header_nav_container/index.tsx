ee3ca47a83e050cdecddd17eaaa0f8750a2f65d2
Israel Mends <israel.mendes@a.unileste.edu.br>
2025-10-12 21:04:42 -0300

    style: Ajust formatation in header components

--- components/header/header_nav/header_nav_container/index.tsx
+++ components/header/header_nav/header_nav_container/index.tsx
@@ -1,9 +1,5 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 
-export function HeaderNavContainer({children}: {children: ReactNode}) {
-    return (
-        <div className="flex flex-col w-full">
-            {children}
-        </div>
-    )
+export function HeaderNavContainer({ children }: { children: ReactNode }) {
+    return <div className="flex flex-col w-full ">{children}</div>;
 }
