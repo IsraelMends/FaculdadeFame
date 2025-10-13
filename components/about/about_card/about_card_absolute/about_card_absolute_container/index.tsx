6efc3cf2bc32a506afe2d0e8755acf835c0cf18c
IsraelMends <iomendes505@gmail.com>
2025-10-13 08:48:47 -0300

    style: Update positioning and spacing in AboutCardAbsoluteContainer, adjust text size in AboutCardAbsoluteDescription and AboutCardAbsoluteTitle components

--- components/about/about_card/about_card_absolute/about_card_absolute_container/index.tsx
+++ components/about/about_card/about_card_absolute/about_card_absolute_container/index.tsx
@@ -3,8 +3,7 @@ import {ReactNode} from "react";
 export function AboutCardAbsoluteContainer({children}: { children: ReactNode }) {
     return (
         <div
-            className="absolute top-1/2 right-[-150px] transform -translate-y-1/2 translate-x-1/4 bg-green-50 p-6
-            md:p-10 rounded-lg shadow-lg w-full sm:w-[400px] max-w-full h-8/12  flex flex-col">
+            className="static md:absolute md:top-1/2 md:right-[-150px] md:transform md:-translate-y-1/2 md:translate-x-1/4 bg-green-50 p-6 md:p-10 rounded-lg shadow-lg w-full sm:w-[400px] max-w-full md:h-8/12 flex flex-col mt-4 md:mt-0">
             {children}
         </div>
     )
