7f7c10b27100bdfe6218b8f0813e45b08001d6fc
Israel Mends <israel.mendes@a.unileste.edu.br>
2025-10-12 21:15:01 -0300

    style: Fix Logo height

--- components/header/header_logo/header_logo_image/index.tsx
+++ components/header/header_logo/header_logo_image/index.tsx
@@ -5,11 +5,11 @@ export function HeaderLogoImage() {
         <Image
             src="/header_logo.png"
             alt="Logo"
-            width={480}
-            height={144}
+            width={600}
+            height={200}
             priority
-            sizes="(max-width: 768px) 160px, (max-width: 1280px) 280px, 320px"
-            className="h-10 w-auto md:h-16 lg:h-20 xl:h-24"
+            sizes="(max-width: 768px) 240px, (max-width: 1280px) 360px, 480px"
+            className="h-16 w-auto md:h-24 lg:h-28 xl:h-32"
         />
     );
 }
