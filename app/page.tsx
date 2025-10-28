34ba1ecf9136662fe4eddea7d11826cb3bfb7288
IsraelMends <iomendes505@gmail.com>
2025-10-28 08:37:53 -0300

    style: Corrigir importação do ContactForm no componente Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
+import React from "react";
 import { Header } from "@/components/header";
 import { Banner } from "@/components/banner";
 // import { Carousel } from "@/components/carousel";
 //import { WhatsAppFloatButton } from "@/components/whatsapp_float_button";
+import ContactForm from "@/components/form";
 import { About } from "@/components/about";
 import { Footer } from "@/components/footer";
-import { ContactForm } from "@/components/form";
-
 export default function Home() {
     return (
         <div className="w-full flex flex-col min-h-screen h-auto">
             <Header />
             <Banner />
             <About />
-            {/* <Carousel/> */}
             <ContactForm />
             <Footer />
             {/* <WhatsAppFloatButton /> */}
